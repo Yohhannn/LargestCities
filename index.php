7380514eb6ca6374202a5722d72faf41569df895
Yohhan <53113613+Yohhannn@users.noreply.github.com>
2025-02-11 11:46:50 +0800

    first commit

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-$weather_conditions = array("rain", "sunshine", "clouds", "hail", "sleet", "snow", "wind");
+$cities = [
+    "Tokyo", "Mexico City", "New York City", "Mumbai", "Seoul", 
+    "Shanghai", "Lagos", "Buenos Aires", "Cairo", "London"
+];
+
+echo implode(", ", $cities) . "<br><br>";
+
+sort($cities);
+echo "<ul>";
+foreach ($cities as $city) {
+    echo "<li>$city</li>";
+}
+echo "</ul>";
+
+$cities = array_merge($cities, ["Los Angeles", "Calcutta", "Osaka", "Beijing"]);
+
+sort($cities);
+echo "<ul>";
+foreach ($cities as $city) {
+    echo "<li>$city</li>";
+}
+echo "</ul>";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weather Report</title>
-</head>
-<body>
-    <h1>Weather Report</h1>
-    <p>
-        <?php
-        echo "We've seen all kinds of weather this month. At the beginning of the month, we had " 
-            . $weather_conditions[5] . " and " . $weather_conditions[6] . ". ";
-        echo "Then came " . $weather_conditions[1] . " with a few " . $weather_conditions[2] . " and some " . $weather_conditions[0] . ". ";
-        echo "At least we didn't get any " . $weather_conditions[3] . " or " . $weather_conditions[4] . ".";
-        ?>
-    </p>
-</body>
-</html>
